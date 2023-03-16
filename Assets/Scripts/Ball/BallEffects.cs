407dcede871bcbbbfa438bf89259a3b253490852
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-16 23:21:00 +0300

    Added Ball Collision Particles

---
+++ Assets/Scripts/Ball/BallEffects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Ball
+{
+	public class BallEffects : MonoBehaviour
+	{
+		[SerializeField] private ParticleSystem _collisionParticlePrefab;
+		
+		private void OnCollisionEnter(Collision other)
+		{
+			Vector3 CollisionPosition = other.contacts[0].point;
+			Instantiate(_collisionParticlePrefab, CollisionPosition, quaternion.identity);
+		}
+	}
+}
