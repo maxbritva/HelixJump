e529137f0c978f8372c7b06d0ce8f3c5e5eb373c
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-18 22:44:13 +0300

    ыщьу

---
+++ Assets/Scripts/Ball/BallBounce.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Physics;
+using UnityEngine;
+
+namespace Ball
+{
+	public class BallBounce : MonoBehaviour
+	{
+		[SerializeField] private Rigidbody _rigidbody;
+		private Bounce _bounce;
+
+		private void Start()
+		{
+			_bounce = new Bounce(_rigidbody);
+		}
+	}
+}
---
+++ Assets/Scripts/Physics/Bounce/Bounce.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Physics
+{
+	public class Bounce
+	{
+		private readonly Rigidbody _rigidBody;
+		private  readonly float _force;
+		private  readonly float _maxHeight;
+
+		public Bounce(Rigidbody rigidBody, float force, float maxHeight)
+		{
+			_rigidBody = rigidBody;
+			_force = force;
+			_maxHeight = maxHeight;
+		}
+
+		public void BounceOff(Vector3 direction) => 
+			_rigidBody.AddForce(direction * _force, ForceMode.VelocityChange);
+
+		public void ClampHeight()
+		{
+			
+		}
+	}
+	
+	public class BounceData 
+	{
+	[SerializeField] private  float _force;
+	[SerializeField] private  float _maxHeight;
+
+	public float Force => _force;
+
+	public float MaxHeight => _maxHeight;
+	}
+}
